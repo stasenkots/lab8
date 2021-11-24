92226c7b46bb0a8448b3e26d9015806e6ca051e2
Anna Graneva <granevaanna7@gmail.com>
2021-11-24 21:11:13 +0300

    update

--- lab8/src/test/java/SkyscannerHotelButtonTest.java
+++ lab8/src/test/java/SkyscannerHotelButtonTest.java
@@ -33,6 +33,6 @@ public class SkyscannerHotelButtonTest {
     @AfterMethod(alwaysRun = true)
     public void browserTearDown(){
         driver.quit();
-        driver = null;
+        //driver = null;
     }
 }
